71a1af840e81ee998b0cb79f2a8610715ce0b5f6
yakir fitousi <yakafi2c@gmail.com>
2019-12-03 16:49:51 +0200

    fix cors

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const path = require("path");
 const express = require("express");
 const app = express();
+const proxy =require('http-proxy-middleware');
 
+app.use(
+  'dataservice.accuweather.com/',
+  proxy('**',{ target: 'localhost:8080', changeOrigin: true })
+);
 app.use(express.static(__dirname + '/dist/weater-app-final/'));
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, '/dist/weater-app-final/index.html'));
